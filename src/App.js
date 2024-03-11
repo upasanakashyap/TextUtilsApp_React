07e385d3eac22f846e3a96d40914987688ea1130
upasanakashyap <upasanakashyap898@gmail.com>
2024-03-11 21:47:50 +0530

    update

--- src/App.js
+++ src/App.js
@@ -1,20 +1,51 @@
-import './App.css';
+import "./App.css";
+import React, { useState } from "react";
 import Navbar from "./components/Navbar";
-import TextForm from "./components/TextForm"
-import About from "./components/About"
+import TextForm from "./components/TextForm";
+import Alert from "./components/Alert"
+//import About from "./components/About";
+
 function App() {
+  let[mode, setMode] = useState('light');
+  const[alert, setAlert]=useState(null);
+
+   const showAlert=(message,type)=>{
+    setAlert({
+      msg:message,
+      type:type
+    })
+    setTimeout(()=>{
+      setAlert(null);
+    },2000)
+  }
+  const  toggleMode=()=>{
+    if(mode ==='light')
+    {
+      setMode('dark');
+      document.body.style.backgroundColor='#042743'
+      showAlert("Dark mode has been enabled","success")
+      document.title ='TextUtils - Dark Mode';
+    }
+    else
+    {
+      setMode('light');
+      document.body.style.backgroundColor='white'
+      showAlert("Light mode has been enabled","success")
+      document.title ='TextUtils - Light Mode';
+    }
+    }
+  
   return (
     <>
-   <Navbar title ="TextUtils 2" 
-   aboutText="About TextUtlis"/>
-   <div className="container my-3">
-
-     {/* <TextForm heading="Enter the text to analyze below"/> */}
-     <About/>
-   </div>
-   
-     </>
+      <Navbar title="TextUtils" mode={mode}  toggleMode={toggleMode}/>
+      <div className="container my-3">
+        <Alert alert={alert}/>
+        <TextForm  showAlert ={showAlert} heading="Enter the text to analyze below"   mode={mode} />
+      </div>
+    </>
   );
-} 
+}
 
 export default App;
+
+
--- src/components/TextForm.js
+++ src/components/TextForm.js
@@ -1,49 +1,55 @@
 import React, {useState} from 'react'
 
-
-
 export default function TextForm(props) {
    const handleUpClick=()=>{
         //console.log("UpperCase was clicked" + text);
         let newText=text.toUpperCase();
         setText(newText)
+        props.showAlert("Converted to UpperCase","success")
    }
 
    const handleLoClick=()=>{
     //console.log("UpperCase was clicked" + text);
     let newText=text.toLowerCase();
     setText(newText)
+    props.showAlert("Converted to LowerCase","success")
   }
 
   const handleClearClick=()=>{
     //console.log("UpperCase was clicked" + text);
     let newText=" ";
     setText(newText)
+    props.showAlert("Text cleared ","success")
    }
 
    const handleOnChange=(event)=>{
     //console.log("On Changed");
     setText(event.target.value);
+    
  }
 
     const[text,setText] =useState(" ");
 
   return (
-    <div>
+    <>
+    <div className='container'  style={{color: props.mode==='dark'?'white':'#042743'}}>
       <h1>{props.heading}</h1>
      <div className ="mb-3">
-         <textarea className="form-control" value={text} onChange={handleOnChange} id="mybox" rows="8"></textarea>
+         <textarea className="form-control" value={text} onChange={handleOnChange} style={{backgroundColor: props.mode==='dark'?'grey':'white',
+         color:props.mode==='dark'?'white':'#042743'}} id="mybox" rows="8"></textarea>
+         
       </div>
       <button className="btn btn-primary mx-2" onClick={handleUpClick}>Convert to UpperCase</button>
       <button className="btn btn-primary mx-2" onClick={handleLoClick}>Convert to UpperCase</button>
       <button className="btn btn-primary mx-2" onClick={handleClearClick}>Cleartext</button>
-       <div className="container my-3">
+      </div>
+       <div className="container my-3" style={{color: props.mode==='dark'?'white':'#042743'}}>
               <h2> Your text Summary</h2>
              <p>{text.split(" ").length}words and{text.length} character</p>
              <p>{0.08 *text.split(" ").length}Minutes read</p>
              <h3>Preview</h3>
-             <p>{text}</p>
+             <p>{text.lenght>0?text:"Enter Something in textbox above to preview it here"}</p>
        </div>
-  </div>
+      </>
   )
 }
